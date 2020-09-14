e541b76dd95bffa42a0c13cf2629cb6c1f6bafba
Adeline BENOIT <adbenoit@e1r4p14.42.fr>
2020-09-14 17:03:03 +0200

    + unset

--- srcs/cmd/ft_unset.c
+++ srcs/cmd/ft_unset.c
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_unset.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/14 17:01:45 by adbenoit          #+#    #+#             */
+/*   Updated: 2020/09/14 17:01:46 by adbenoit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 void    ft_unset(t_stock **cmd_lst, char *envp[])
 {
     int     i;
+    int     j;
 
     i = 0;
     while ((*cmd_lst)->input[i])
@@ -14,5 +27,12 @@ void    ft_unset(t_stock **cmd_lst, char *envp[])
         }
         ++i;
     }
-    i = find_var(envp, (*cmd_lst)->input); 
+    i = find_var(envp, (*cmd_lst)->input);
+    j = i;
+    while (envp[++j])
+    {
+        ft_strcpy(envp[i], envp[j]);
+        ++i;
+    }
+    envp[i] = 0;
 }
