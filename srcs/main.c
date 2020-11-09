ebef43fd449a9bcc86e402e64c3c6278ff382882
Adeline Benoit <Adeline@MacBook-Air-de-Adeline.local>
2020-11-09 12:50:07 +0100

    little merge

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/25 15:52:05 by mabriand          #+#    #+#             */
-/*   Updated: 2020/11/09 12:45:15 by adbenoit         ###   ########.fr       */
+/*   Updated: 2020/11/09 12:48:02 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ void	signal_handler(int signo)
  		write(2, "\xF0\x9F\x90\x9A: ", 6);
 		signal(SIGINT, signal_handler);
 	}
+	else if (signo == SIGQUIT)
+	{
+		printf("MAIS WTF\n\n");
+	}
 	printf("NO SIGINT\n");
 }
 
@@ -43,10 +47,10 @@ int main(int argc, char *argv[], char *envp[])
 	while (1)
 	{
 		signal(SIGQUIT, signal_handler);
+		signal(SIGINT, signal_handler);
 		write(1, "\033[1mLesPetitsCoquillages\033[0m", 29);
 		write(1, "\xF0\x9F\x90\x9A: ", 6);
 		str = NULL;
-		errno = 0;
 		ret = get_next_line(0, &str);
 		g_cmd = NULL;
 		i = 0;
@@ -56,7 +60,6 @@ int main(int argc, char *argv[], char *envp[])
 		{
 			parsing(str, envp);
 			ft_stockclear(&g_cmd, clear_one);
-			free(g_cmd);
 		}
 		free(str);
 	}
