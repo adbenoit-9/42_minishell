51bfde60c82d8356b6a4e9c3fd95677d47e18dde
Adeline Benoit <Adeline@MacBook-Air-de-Adeline.local>
2020-12-31 00:35:23 +0100

    leaks?

--- srcs/util/ft_realloc.c
+++ srcs/util/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/29 16:41:55 by adbenoit          #+#    #+#             */
-/*   Updated: 2020/12/22 20:39:04 by adbenoit         ###   ########.fr       */
+/*   Updated: 2020/12/31 00:33:20 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,12 @@ void	*ft_realloc(void *ptr, int newsize)
 
 	if (ptr == 0)
 		return (malloc(newsize));
-	newptr = malloc(newsize);
+	if (!(newptr = malloc(newsize)))
+	{
+		free(ptr);
+		ptr = NULL;
+		return (NULL);
+	}
 	size = ft_strlen(ptr);
 	if (newsize > size)
 		ft_memcpy(newptr, ptr, newsize);
