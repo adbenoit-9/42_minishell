12c93fc88324d992ea225e62aa6b6b096be0e089
Adeline Benoit <Adeline@MacBook-Air-de-Adeline.local>
2020-12-31 01:05:26 +0100

    re realloc tab

--- srcs/util/ft_realloc.c
+++ srcs/util/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/29 16:41:55 by adbenoit          #+#    #+#             */
-/*   Updated: 2020/12/31 01:03:23 by adbenoit         ###   ########.fr       */
+/*   Updated: 2020/12/31 01:05:14 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,19 +35,16 @@ char	**ft_realloc_tab(char **ptr, int newsize)
 	int		i;
 	int		size;
 
-	i = 0;
 	if (ptr == 0)
 		return ((char **)malloc(sizeof(char *) * newsize));
 	size = ft_tabsize(ptr);
 	if (size >= newsize)
 		return (ptr);
 	newptr = (char **)malloc(sizeof(char *) * newsize);
-	while (i < size)
-	{
+	i = -1;
+	while (newptr && ++i < size)
 		if (!(newptr[i] = ft_strdup(ptr[i])))
 			break ;
-		++i;
-	}
 	if (i != size)
 	{
 		ft_free(newptr);
