c34901696ff3385dd969112fb10e5b52146618cc
Adeline BENOIT <adbenoit@e1r10p8.42.fr>
2021-01-08 15:45:19 +0100

    clear

--- libft/src/str/ft_putendl_fd.c
+++ libft/src/str/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 19:28:15 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/01/06 02:06:10 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/01/08 15:43:54 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,8 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	size_t	i;
 	size_t	len;
 
-	i = 0;
 	if (s)
 	{
 		len = ft_strlen(s);
